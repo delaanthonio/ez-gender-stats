6a35959deb29ad161210a0a09c97e8b981637309
Dela Anthonio <dell.anthonio@gmail.com>
2018-12-30 17:58:11 -0500

    [feature] Add Script for Importing Data Into HDFS From MySQL

--- scripts/import_all.sh
+++ scripts/import_all.sh
@@ -1,51 +1,15 @@
-#! /bin/bash
+#!/usr/bin/env bash
+bqs=("BQ1" "BQ2" "BQ3" "BQ4" "BQ5")
 
-# Business Question 1
+for bq in "${bqs[@]}"; do
 sqoop import \
---connect jdbc:mysql://localhost/GENDER_STATS_DB \
---username EZ_ADMIN \
+--connect 'jdbc:mysql://localhost/GENDER_STATS_DB' \
+--username 'EZ_ADMIN' \
 --password 'ez--__!1978!' \
---table BQ1 \
+--table "${bq}" \
 --fields-terminated-by '|' \
---target-dir EZData/BQ1/ \
+--target-dir "EZData/${bq}/" \
 -m 1
+done
 
-# Business Question 2
-sqoop import \
---connect jdbc:mysql://localhost/GENDER_STATS_DB \
---username EZ_ADMIN \
---password 'ez--__!1978!' \
---table BQ2 \
---fields-terminated-by '|' \
---target-dir EZData/BQ2/ \
--m 1
-
-# Business Question 3
-sqoop import \
---connect jdbc:mysql://localhost/GENDER_STATS_DB \
---username EZ_ADMIN \
---password 'ez--__!1978!' \
---table BQ3 \
---fields-terminated-by '|' \
---target-dir EZData/BQ3/ \
--m 1
-
-# Business Question 4
-sqoop import \
---connect jdbc:mysql://localhost/GENDER_STATS_DB \
---username EZ_ADMIN \
---password 'ez--__!1978!' \
---table BQ4 \
---fields-terminated-by '|' \
---target-dir EZData/BQ4/ \
--m 1
-
-# Business Question 5
-sqoop import \
---connect jdbc:mysql://localhost/GENDER_STATS_DB \
---username EZ_ADMIN \
---password 'ez--__!1978!' \
---table BQ5 \
---fields-terminated-by '|' \
---target-dir EZData/BQ5/ \
--m 1
+exit
