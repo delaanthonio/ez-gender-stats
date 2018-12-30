b46609aaf3b57f876a249e1067e626d2739a6373
Dela Anthonio <dell.anthonio@gmail.com>
2018-12-30 17:59:45 -0500

    [fix]  Convert Line Endings in export_all.sh to Unix

--- scripts/export_all.sh
+++ scripts/export_all.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 sqoop export \
-      --connect jdbc:mysql://localhost/GENDER_STATS_DB \
-      --username EZ_ADMIN \
+      --connect "jdbc:mysql://localhost/GENDER_STATS_DB" \
+      --username "EZ_ADMIN" \
       --password 'ez--__!1978!' \
-      --table GENDER_STATS_DATA \
+      --table "GENDER_STATS_DATA" \
       --fields-terminated-by ',' \
       --enclosed-by '\"' \
-      --export-dir GenderStatsData/ \
-      -m 1
+      --export-dir "GenderStatsData" \
+      -m "1"
