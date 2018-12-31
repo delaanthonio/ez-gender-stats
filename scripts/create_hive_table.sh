6e9e332ecf5292c55b5a6e222bdaded65d4905ff
Dela Anthonio <dell.anthonio@gmail.com>
2018-12-30 20:57:10 -0500

    [feature] Add Support for Creating Tables for Pig

--- scripts/create_hive_table.sh
+++ scripts/create_hive_table.sh
@@ -2,10 +2,12 @@
 bqs=("BQ1" "BQ2" "BQ3" "BQ4" "BQ5")
 
 for bq in "${bqs[@]}"; do
-    hive_table_path="Project2/hive/tables/${bq}"
-    echo "$hive_table_path"
-    hdfs dfs -mkdir -p "$hive_table_path"
-    hdfs dfs -cp "EZData/${bq}/part-m-00000" "$hive_table_path"
+    tables=("Project2/hive/tables/${bq}" "Project2/pig/${bq}")
+    for table in "${tables[@]}"; do
+        echo "Creating ${table}"
+        hdfs dfs -mkdir -p "${table}"
+        hdfs dfs -cp "EZData/${bq}/part-m-00000" "${table}"
+    done
 done
 
 exit
